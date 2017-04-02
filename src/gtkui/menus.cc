d8b25248ad5027586ad5a7c82295969e1bdc10b1
landroni <landronimirc@gmail.com>
2017-04-02 21:41:03 +0200

    rename Stop After Current Song menu item

--- src/gtkui/menus.cc
+++ src/gtkui/menus.cc
@@ -99,7 +99,7 @@ static const AudguiMenuItem playback_items[] = {
     MenuToggle (N_("S_huffle"), nullptr, 's', CTRL, nullptr, "shuffle", nullptr, "set shuffle"),
     MenuToggle (N_("Shuffle by Albu_m"), nullptr, NONE, nullptr, "album_shuffle", nullptr, "set album_shuffle"),
     MenuToggle (N_("N_o Playlist Advance"), nullptr, 'n', CTRL, nullptr, "no_playlist_advance", nullptr, "set no_playlist_advance"),
-    MenuToggle (N_("Stop A_fter This Song"), nullptr, 'm', CTRL, nullptr, "stop_after_current_song", nullptr, "set stop_after_current_song"),
+    MenuToggle (N_("Stop A_fter Current Song"), nullptr, 'm', CTRL, nullptr, "stop_after_current_song", nullptr, "set stop_after_current_song"),
     MenuSep (),
     MenuCommand (N_("Song _Info ..."), "dialog-information", 'i', CTRL, audgui_infowin_show_current),
     MenuCommand (N_("Jump to _Time ..."), "go-jump", 'k', CTRL, audgui_jump_to_time),
